2706eb369e42156005cdf00d5858c7525d980d31
husakev <husakev@ukr.net>
2017-08-31 20:31:00 +0300

    отрабатывает норм но выдает ошибку

---
+++ src/moduleFive1/Additionali.java
@@ -0,0 +1,118 @@
+package moduleFive1;
+
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.Pane;
+import javafx.scene.paint.Color;
+import javafx.scene.paint.Paint;
+import javafx.scene.shape.Circle;
+import javafx.stage.Stage;
+
+import java.util.Arrays;
+
+import static moduleFive1.Window.*;
+
+/**
+ * Created by Евгений on 29.08.2017.
+ */
+/*  Дополнительные задания
+    Убрать ввод с консоли и заменить его на UI элементы (кнопки и поля для ввода)
+    Добавить кнопку - покрасить все круги в красный. Она красит все ранее нарисованные круги
+     в красный цвет.
+    Добавить кнопку - Gradient. Она красит все круги начиная от нижнего к верхнему серыми
+оттенками. К примеру нижний круг полностью черный, а каждый круг выше становиться более
+светлым. И в конечном счете верхний круг снеговика должен быть светло серым. */
+public class Additionali extends Application {
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    public static void circleSort() {
+        double[] sort = new double[Snowman.count];
+        for (int i = 0; i < Snowman.count; i++) {
+            sort[i] = Double.parseDouble(String.valueOf(circle[i]));
+        }
+        Arrays.sort(sort);
+        for (int i = Snowman.count; i >= 0; i--) {
+            circle[i].setRadius(sort[i]);
+        }
+    }
+
+    public static Circle[] circleRgb(int count) {
+        double rgb = 0.0;
+        double sum = 0.9 / (count - 1);
+        for (int i = 0; i < count; i++) {
+            Color color = Color.color(rgb, rgb, rgb);
+            rgb = sum + rgb;
+            circle[i].setFill(Paint.valueOf(color.toString()));
+        }
+        return circle;
+    }
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        primaryStage.setTitle("Snowman");
+        Pane pane = new Pane();
+        TextField count = new TextField();
+        TextField min = new TextField();
+        TextField max = new TextField();
+        count.textFormatterProperty();
+        Button buttonNext = new Button("Next");
+        Button buttonRed = new Button("Red");
+        Button buttonGradient = new Button("Gradient");
+        count.setLayoutX(5);
+        count.setLayoutY(5);
+        min.setLayoutX(5);
+        min.setLayoutY(35);
+        max.setLayoutX(5);
+        max.setLayoutY(65);
+        buttonNext.setTranslateX(500);
+        buttonNext.setTranslateY(100);
+        buttonRed.setTranslateX(500);
+        buttonRed.setTranslateY(550);
+        buttonGradient.setTranslateX(500);
+        buttonGradient.setTranslateY(600);
+        scene = new Scene(pane, wind, wind);
+        javafx.scene.text.Text textCount = new javafx.scene.text.Text("- Введите " +
+                "количество кругов, от 3  до 6");// я не понял как я это написал
+        javafx.scene.text.Text textMin = new javafx.scene.text.Text("- Введите" +
+                " минимальный радиус кругов, от 15  до 35");
+        javafx.scene.text.Text textMax = new javafx.scene.text.Text("- Введите " +
+                "максимальный радиус кругов, от 40  до 80");
+        textCount.setX(160);
+        textCount.setY(20);
+        textMin.setX(160);
+        textMin.setY(50);
+        textMax.setX(160);
+        textMax.setY(80);
+
+        buttonNext.setOnAction((event) -> {
+            pane.getChildren().clear();
+            Snowman.count = Integer.parseInt(count.getText());
+            Snowman.min = Integer.parseInt(min.getText());
+            Snowman.max = Integer.parseInt(max.getText());
+            pane.getChildren().addAll(circle(Snowman.count));
+            pane.getChildren().addAll(Window.face(3));
+            pane.getChildren().addAll(buttonRed);
+            pane.getChildren().addAll(buttonGradient);
+            buttonRed.setOnAction(event1 -> {
+
+                for (int i = 0; i < Snowman.count; i++) {
+                    circle[i].setFill(Color.RED);
+                }
+            });
+            buttonGradient.setOnAction(event1 -> {
+                circleRgb(Snowman.count);
+            });
+            circleSort();
+            primaryStage.setScene(scene);
+        });
+        primaryStage.setScene(scene);
+        pane.getChildren().addAll(count, min, max, textCount, textMin, textMax, buttonNext);
+        primaryStage.show();
+
+    }
+}
--- src/moduleFive1/Window.java
+++ src/moduleFive1/Window.java
@@ -9,6 +9,7 @@ import javafx.scene.paint.Paint;
 import javafx.scene.shape.Circle;
 import javafx.stage.Stage;
 
+import static moduleFive1.Snowman.*;
 /**
  * Created by Евгений on 05.08.2017.
  */
@@ -24,7 +25,7 @@ import javafx.stage.Stage;
 public class Window extends Application {
     static double wind = 680;
     static Circle[] circle;
-
+    static Scene scene;
     public static void main(String... args) {
         launch(args);
     }
@@ -35,15 +36,15 @@ public class Window extends Application {
         Color color = Color.color(Math.random(), Math.random(), Math.random());
         circleF[0] = new Circle(wind / 2,
                 circle[circle.length - 1].getCenterY() + (radius / 3),
-                5 + Math.random() * (radius / 3),
+                3 + Math.random() * (radius / 4),
                 Paint.valueOf(color.toString()));
         circleF[1] = new Circle((wind / 2) - (radius / 3),
                 circle[circle.length - 1].getCenterY() - (radius / 3),
-                3 + Math.random() * (radius / 5),
+                2 + Math.random() * (radius / 6),
                 Paint.valueOf(color.toString()));
         circleF[2] = new Circle((wind / 2) + (radius / 2),
                 circle[circle.length - 1].getCenterY() - (radius / 3),
-                3 + Math.random() * (radius / 5),
+                2 + Math.random() * (radius / 6),
                 Paint.valueOf(color.toString()));
         return circleF;
     }
@@ -51,12 +52,12 @@ public class Window extends Application {
     public static Circle[] circle(int count) {
         circle = new Circle[count];
         for (int i = 0; i < circle.length; i++) {
-            double radius = Snowman.min + Math.random() * Snowman.max;
+            double radius = min + Math.random() * max;
             Color color = Color.color(Math.random(), Math.random(), Math.random());
             if (i == 0) {
-                circle[i] = new Circle(wind / 2, wind - radius - 39, radius,
-                        Paint.valueOf(color.toString())); // -39 из-за того что 1 круг перекрывает
-            }                                         // немного. Можете убрать и увидете
+                circle[i] = new Circle(wind / 2, wind - radius, radius,
+                        Paint.valueOf(color.toString()));
+            }
             else {
                 circle[i] = new Circle(wind / 2,
                         circle[i - 1].getCenterY() - circle[i - 1].getRadius()
@@ -69,21 +70,19 @@ public class Window extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception {
         primaryStage.setTitle("Snowman");
-        primaryStage.setWidth(wind);
-        primaryStage.setHeight(wind);
         Pane pane = new Pane();
-        pane.getChildren().addAll(circle(Snowman.count));
+        pane.getChildren().addAll(circle(count));
         pane.getChildren().addAll(face(3));
-        Button button = new Button();
-        Scene scene = new Scene(pane);
-        button.setTranslateX(600);
-        button.setTranslateY(600);
-        button.setText("show");
-        button.setOnMouseClicked(event -> {
+        Button button = new Button("show");
+        scene = new Scene(pane, wind, wind);// если писать primaryStage.setWidth и
+        button.setTranslateX(600); // Height то низ снеговика в открывающимся окне немного
+        button.setTranslateY(600); // закрывался рамкой окна. приходилось отнимать 39 от
+        button.setOnMouseClicked(event -> { // основания, чтоб норм видно было
             pane.getChildren().clear();
-            pane.getChildren().addAll(circle(Snowman.count));
+            pane.getChildren().addAll(circle(count));
             pane.getChildren().addAll(face(3));
             pane.getChildren().addAll(button);
+
         });
         pane.getChildren().addAll(button);
         primaryStage.setScene(scene);
